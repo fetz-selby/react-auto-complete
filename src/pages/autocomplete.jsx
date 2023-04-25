775b6870768010bf55753974b4d1ce7d4c8ccc59
fetz <emmsel@on.sinch.com>
2023-04-25 22:48:54 +0200

    auto complete page added

---
+++ src/pages/autocomplete.jsx
@@ -0,0 +1,9 @@
+import { AutoComplete } from '@/components/AutoComplete';
+
+const AutoCompleteView = () => {
+    return <div>
+        <AutoComplete />
+        </div>;
+}
+
+export default AutoCompleteView;
